d9a8579accbf60009c508d8cc2bb10779d718177
FredrikM2000 <70491657+FredrikM2000@users.noreply.github.com>
2020-09-14 01:43:53 +0200

    Fix win algorithm

--- Tic-tac-toe.cpp
+++ Tic-tac-toe.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 
-char tic[10] = "123456789";
+char position[10] = "123456789";
    void table() {
-        std::cout << "  |  " << tic[0] << "  ";
-        std::cout << "|  " << tic[1] << "  ";
-        std::cout << "|  " << tic[2] << "  |\n";
-        std::cout << "  |  " << tic[3] << "  ";
-        std::cout << "|  " << tic[4] << "  ";
-        std::cout << "|  " << tic[5] << "  |\n";
-        std::cout << "  |  " << tic[6] << "  ";
-        std::cout << "|  " << tic[7] << "  ";
-        std::cout << "|  " << tic[8] << "  |\n";
+        std::cout << "  |  " << position[0] << "  ";
+        std::cout << "|  " << position[1] << "  ";
+        std::cout << "|  " << position[2] << "  |\n";
+        std::cout << "  |  " << position[3] << "  ";
+        std::cout << "|  " << position[4] << "  ";
+        std::cout << "|  " << position[5] << "  |\n";
+        std::cout << "  |  " << position[6] << "  ";
+        std::cout << "|  " << position[7] << "  ";
+        std::cout << "|  " << position[8] << "  |\n";
         std::endl(std::cout);
     }
 int main() {
@@ -26,19 +26,21 @@ int main() {
         std::cout << "Write a number between 1 or 9: ";
         std::cin >> p1;
         int choice = p1 - 1;
-        tic[choice] = 'X';
 
-        
-        
-        /*for i 0 - 2, if i + 3 + 3 exist in X-array win = true
-        for 0,3,6 if i + 1 + 1 exist = win
-        for tic[0] if i + 4 + 4 exist = win
-        for tic[2] if i + 2 + 2 exist = win
-
-        *REMEMBER IF SPOT ALREADY TAKEN NOT VALID*
-
-        */
+        if (position[choice] == 'X' || position[choice] == 'O') {
+            std::cout << "Already taken" << std::endl;;
+        } else {
+            position[choice] = 'X';
+        }
+        for (int i = 0; i <= 7; i++) {
+            for (int j = 1; j <= 4; j++) {
+                if (position[i] == 'X' && position[i + j] == 'X' && position[i + j + j] == 'X') {
+                    win = true;
+                }
+            }    
+        }
     } while (win != true);
     table();
+    std::cout << "Player 1 wins!";
 }
 
