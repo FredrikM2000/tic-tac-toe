2b9f635d05c14a30fed50969d0051aaa5b47b753
FredrikM2000 <70491657+FredrikM2000@users.noreply.github.com>
2020-09-15 18:04:38 +0200

    Huge updates

--- Tic-tac-toe.cpp
+++ Tic-tac-toe.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 
 char position[10] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+bool win = false;
+int currentPlayer = 1;
 void table() {
-        std::cout << "  |  " << position[0] << "  ";
+        std::cout << "\n  |  " << position[0] << "  ";
         std::cout << "|  " << position[1] << "  ";
         std::cout << "|  " << position[2] << "  |\n";
         std::cout << "  |  " << position[3] << "  ";
@@ -13,133 +15,132 @@ void table() {
         std::cout << "|  " << position[8] << "  |\n";
         std::endl(std::cout);
 }
-
-int i;
-int j;
-int a;
-int b;
-bool win = false;
-void check1() {
-    if (position[i] == 'X' && position[i + j] == 'X' && position[i + j + j] == 'X') {
+void check1(int i, int j, char z) {
+    if (position[i] == z && position[i + j] == z && position[i + j + j] == z) {
         win = true;
     }
 }
-void check2() {
-    if (position[a] == 'O' && position[a + b] == 'O' && position[a + b + b] == 'O') {
-        win = true;
+void check2(char z) {
+    for (int n = 0; n <= 6;n++) {
+        if (n % 3 == 0) {
+            check1(n, 1, z);
+        }
+        if (n <= 2) {
+            check1(n, 3, z);
+        }
+        if (n == 2) {
+            check1(n, 2, z);
+        }
+        if (n == 0) {
+            check1(n, 4, z);
+        }
     }
 }
-int main() {
-    int p1;
-    int p2;
-
-    std::cout << "\t******* Welcome to Tic Tac Toe! *******\n\n";
-    bool player1_turn = true;
-    do {
-        table();
-        int X[10] = {};
-
-        if (player1_turn == true) {
-            std::cout << "Player 1, write a number between 1 or 9: ";
-            std::cin >> p1;
-            int choice1 = p1 - 1;
-
-            if (position[choice1] == 'X' || position[choice1] == 'O') {//checks if the spot is already taken
-                std::cout << "Already taken" << std::endl;;
-            }
-            else {
-                position[choice1] = 'X';
-            }
-
-            /*int h;
-            int v;
-            int d;
-
-
-            switch (choice) {
-                case (0):
-                    h = 1;
-                    v = 2;
-                    d = 4;
-                    break;
-                default:
-                    ;
-            }
-
-            int j;
-            int angle[4] = { h, v, d };
-            for (int i = 0; i <= 2; i++) {
-                j = angle[i];
-                std::cout << angle[0];
-                if (position[choice + j] == 'X' && position[choice + j + j] == 'X') {
-                    win = true;
-                    std::cout << i;
-                }
-            }*/
-            for (i = 0; i <= 6; i++) {
-                if (i % 3 == 0) {
-                    j = 1;
-                    check1();
-                    if (i == 0) {
-                        j = 4;
-                        check1();
-                    }
-                }
-                if (i <= 2) {
-                    j = 3;
-                    check1();
-                    if (i == 2) {
-                        j = 2;
-                        check1();
-                    }
-                }
-            }
-        }
-        if (player1_turn == false) {
-            std::cout << "Player 2, write a number between 1 or 9: ";
-            std::cin >> p2;
-            int choice2 = p2 - 1;
-
-            if (position[choice2] == 'X' || position[choice2] == 'O') {//checks if the spot is already taken
-                std::cout << "Already taken" << std::endl;;
-            }
-            else {
-                position[choice2] = 'O';
-            }
-            for (a = 0; a <= 6; a++) {
-                if (a % 3 == 0) {
-                    b = 1;
-                    check2();
-                    if (a == 0) {
-                        b = 4;
-                        check2();
-                    }
-                }
-                if (a <= 2) {
-                    b = 3;
-                    check2();
-                    if (a == 2) {
-                        b = 2;
-                        check2();
-                    }
-                }
-            }
-        }
-        
-        switch (player1_turn) {
-        case true:
-            player1_turn = false;
-            break;
-        case false:
-            player1_turn = true;
-        }
-    }while (win != true);
+void test(char brick) {
+    bool valid_input = false;
+    int player;
     table();
-    if (player1_turn == false) {
-        std::cout << "Player 1 wins!";
-    }
-    else {
-        std::cout << "Player 2 wins!";
-    }
+    do {
+        std::cout << "Player " << currentPlayer << ", write a number between 1 or 9 : ";
+        std::cin >> player;
+        int choice = player - 1;
+
+        if (position[choice] == 'X' || position[choice] == 'O') {//checks if the spot is already taken
+            std::cout << "Already taken" << std::endl;
+        }
+        else {
+            position[choice] = brick;
+            valid_input = true;
+        }
+    } while (valid_input == false);
 }
+
+
+
+//************************REMEMBER TO COMMENT TO EXPLAIN ALL THIS SHIT*****************
+/*things you could do:
+-color the X an O red
+
+
+
+*/ 
+int main() {
+
+    std::cout << "\t******* Welcome to Tic Tac Toe! *******\n";
+
+    
+
+    bool valid_selection = true;
+    do {
+        int selection;
+        std::cout << "Select option : \n";
+        std::cout << "Play 1v1(1)\n";
+        std::cout << "Play against AI(2)\n";
+        std::cin >> selection;
+
+        if (selection == 1) {
+            valid_selection = true;
+            bool player1_turn = true;
+            do {
+                int X[10] = {};
+
+                if (player1_turn == true) {
+                    test('X');
+                    check2('X');
+                }
+                if (player1_turn == false) {
+                    test('O');
+                    check2('O');
+                }
+                switch (player1_turn) {
+                case true:
+                    player1_turn = false;
+                    currentPlayer = 2;
+                    break;
+                case false:
+                    player1_turn = true;
+                    currentPlayer = 1;
+                }
+            } while (win != true);
+
+            table();
+            if (player1_turn == false) {
+                std::cout << "Player 1 wins!";
+            }
+            else {
+                std::cout << "Player 2 wins!";
+            }
+        }
+        else if (selection == 2) {
+            valid_selection = true;
+
+            int r = 0;
+            do {
+                test('X');
+                check2('X');
+
+
+
+
+
+
+
+                position[r] = 'O';
+                r++;
+            } while (win != true);
+
+
+
+
+
+
+
+        } else {
+            std::cout << "Invalid option, try again";
+            valid_selection = false;
+        }
+    }while (valid_selection == false);
+    table();
+}
+
 
